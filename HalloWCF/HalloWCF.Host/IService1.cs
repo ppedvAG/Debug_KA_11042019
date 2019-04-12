876467100ceaabaf829016a18ac8bf6cd5de1b66
André <andrerrr@outlook.de>
2019-04-12 10:05:01 +0200

    WCF

---
+++ HalloWCF/HalloWCF.Host/IService1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HalloWCF.Host
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        string GetWochentag();
+
+        [OperationContract]
+        IEnumerable<Kuchen> GetKuchen();
+    }
+
+    [DataContract]
+    public class Kuchen
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        public int ZuckerInKg { get; set; }
+
+        [DataMember]
+        public int Druchmesser { get; set; }
+
+        [DataMember]
+        public int MyProperty { get; set; }
+    }
+
+}
---
+++ HalloWCF/HalloWCF.Host/Service1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HalloWCF.Host
+{
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public IEnumerable<Kuchen> GetKuchen()
+        {
+            yield return new Kuchen()
+            {
+                Name = "Käsekuchen",
+                Druchmesser = 27,
+                ZuckerInKg = 1
+            };
+            yield return new Kuchen()
+            {
+                Name = "Apfelkuchen",
+                Druchmesser = 12,
+                ZuckerInKg = 2
+            };
+            yield return new Kuchen()
+            {
+                Name = "Donauwelle",
+                Druchmesser = 4,
+                ZuckerInKg = 3
+            };
+        }
+
+        public string GetWochentag()
+        {
+            return DateTime.Now.ToString("dddd");
+        }
+    }
+}
